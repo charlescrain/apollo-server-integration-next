3feab6afce197efe67db5706dc041de87201649b
Luke Whyte <lukeawhyte@gmail.com>
2024-04-16 21:22:04 -0500

    Still cors issues

--- src/pages/api/graphql/index.ts
+++ src/pages/api/graphql/index.ts
@@ -3,7 +3,6 @@ import resolvers from './resolvers/main'
 import typeDefs from './schema/main'
 import { startServerAndCreateNextHandler } from '../../../startServerAndCreateNextHandler'
 import { NextApiHandler } from 'next'
-import NextCors from 'nextjs-cors'
 
 const server = new ApolloServer({
   resolvers,
@@ -13,10 +12,17 @@ const server = new ApolloServer({
 const handler = startServerAndCreateNextHandler(server)
 
 const graphql: NextApiHandler = async (req, res) => {
-  await NextCors(req, res, {
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-  })
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader('Access-Control-Allow-Headers', '*')
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  }
 
   await handler(req, res)
 }
