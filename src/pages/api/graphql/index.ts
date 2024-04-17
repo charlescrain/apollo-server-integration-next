6fbb16e0fff21ba0960bf2df3062c095660c07ef
Luke Whyte <lukeawhyte@gmail.com>
2024-04-16 19:33:41 -0500

    Fixing cors issue

--- src/pages/api/graphql/index.ts
+++ src/pages/api/graphql/index.ts
@@ -3,6 +3,8 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import resolvers from './resolvers/main'
 import typeDefs from './schema/main'
 import { startServerAndCreateNextHandler } from '../../../startServerAndCreateNextHandler'
+import { NextApiHandler } from 'next'
+import NextCors from 'nextjs-cors'
 
 const server = new ApolloServer({
   resolvers,
@@ -11,4 +13,15 @@ const server = new ApolloServer({
   plugins: [ApolloServerPluginLandingPageLocalDefault()],
 })
 
-export default startServerAndCreateNextHandler(server)
+const handler = startServerAndCreateNextHandler(server)
+
+const graphql: NextApiHandler = async (req, res) => {
+  await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+  })
+
+  await handler(req, res)
+}
+
+export default graphql
